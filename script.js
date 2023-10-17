8dac98f04ee7a575a51c8ad4cadc256680ef6a8d
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-17 17:26:48 +0200

    Studying classes...

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-function Enemy(name, level, emoji, attackName) {
-    this.name = name;
-    this.level = level;
-    this.emoji = emoji;
-    this.attackName = attackName;
-    this.attack = function() {
+class Enemy {
+    constructor(name, level, emoji, attackName) {
+        this.name = name;
+        this.level = level;
+        this.emoji = emoji;
+        this.attackName = attackName;
+    }
+
+    attack() {
         return `${this.name} ${this.attackName}!`;
     }
 }
 
-const enemy1 = new Enemy('Rat', 1, '🐀', 'Bites');
+const enemies = [
+    new Enemy('Goblin', 1, '👺', 'punches'),
+    new Enemy('Troll', 5, '👹', 'bites'),
+    new Enemy('Dragon', 10, '🐲', 'burns'),
+    new Enemy('Demon', 20, '😈', 'eats'),
+    new Enemy('Satan', 50, '👿', 'destroys')
+];
 
-console.log(enemy1.attack()); // Rat Bites!
+const enemy = enemies[Math.floor(Math.random() * enemies.length)];
 
-function Heroes(name, level, weapon) {
-    this.name = name;
-    this.level = level;
-    this.weapon = weapon;
-    this.attack = function() {
+console.log(enemy.attack());
+
+class Hero {
+    constructor(name, level, weapon) {
+        this.name = name;
+        this.level = level;
+        this.weapon = weapon;
+    }
+
+    attack() {
         return `${this.name} attacks with ${this.weapon}!`;
+    }
+
+    equipWeapon(newWeapon) {
+        this.weapon = newWeapon;
     }
 }
 
-const hero1 = new Heroes('Bjorn', 1, 'axe');
-const hero2 = new Heroes('Layla', 1, 'bow');
+const heroes = [
+    new Hero('Warrior', 1, 'sword'),
+    new Hero('Valkyrie', 5, 'axe'),
+    new Hero('Wizard', 10, 'staff'),
+    new Hero('Elf', 20, 'bow'),
+    new Hero('Dwarf', 50, 'hammer')
+];
 
-console.log(hero1.attack()); // Bjorn attacks with axe!
-console.log(hero2.attack()); // Layla attacks with bow!
+const hero = heroes[Math.floor(Math.random() * heroes.length)];
+
+console.log(hero.attack());
