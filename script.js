728ab317ad1377c7df3946d420cc0c10f3137e0c
Ange230700 <kouakouangeericstephane@gmail.com>
2023-10-17 17:15:46 +0200

    Using constructor function part 2

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function Enemy(name, level, emoji, attackName) {
 const enemy1 = new Enemy('Rat', 1, '🐀', 'Bites');
 
 console.log(enemy1.attack()); // Rat Bites!
+
+function Heroes(name, level, weapon) {
+    this.name = name;
+    this.level = level;
+    this.weapon = weapon;
+    this.attack = function() {
+        return `${this.name} attacks with ${this.weapon}!`;
+    }
+}
+
+const hero1 = new Heroes('Bjorn', 1, 'axe');
+const hero2 = new Heroes('Layla', 1, 'bow');
+
+console.log(hero1.attack()); // Bjorn attacks with axe!
+console.log(hero2.attack()); // Layla attacks with bow!
